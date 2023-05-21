79db21f76d332e17e378e331311d40c3d96d6043
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-05-21 21:34:07 +0530

    Added Chatbot

--- CloudClinic-develop/frontend/src/components/Main/Main.js
+++ CloudClinic-develop/frontend/src/components/Main/Main.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Main.scss';
 
 const Main = ({ children }) => {
+
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"21f204744aa7faffb7ca76f75c655da11","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+
   return <div className="main-wrapper">{children}</div>;
 };
 
